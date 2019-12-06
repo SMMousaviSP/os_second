0375379e334d21518a668e738132e3905e307bbb
SMMousaviSP <smmousavisp@gmail.com>
2019-12-07 01:35:15 +0330

    Complete sem.py

--- producer_consumer/sem.py
+++ producer_consumer/sem.py
@@ -1,51 +1,51 @@
-import multiprocessing as mp
+# import multiprocessing as mp
 import threading
+from concurrent.futures import ThreadPoolExecutor
 import random
 import time
 
 def producer():
-    global empty
-    # global mutex
-    # global buffer
-    # global full
-    # while True:
-    time.sleep(random.randrange(3, 6))
-    item = random.randrange(1, 1000)
-    empty.acquire()
-    mutex.acquire()
-    buffer.append(item)
-    mutex.release()
-    full.release()
+    global buffer
+    while True:
+        time.sleep(1)
+        item = random.randrange(1, 1000)
+        print("producer: try to acquire empty")
+        empty.acquire()
+        print("producer: empty acquired, try to acquire mutex")
+        mutex.acquire()
+        print("producer: mutex acquired")
+        print(f"producer: produce: {item}")
+        buffer.append(item)
+        mutex.release()
+        print("producer: mutex released")
+        full.release()
+        print("producer: full released")
 
 def consumer():
-    global empty
-    # global mutex
-    # global buffer
-    # global full
-    # while True:
-    time.sleep(random.randrange(3, 6))
-    full.acquire()
-    mutex.acquire()
-    item = buffer.pop()
-    mutex.release()
-    empty.release()
-    print(f"consume {item}")
+    print("consumer: IN")
+    global buffer
+    while True:
+        time.sleep(1)
+        print("consumer: try to acquire full")
+        full.acquire()
+        print("consumer: full acquired, try to acquire mutex")
+        mutex.acquire()
+        item = buffer.pop()
+        mutex.release()
+        print("consumer: mutex released")
+        empty.release()
+        print("consumer: empty released")
+        print(f"consumer: consume: {item}")
 
 
+
+
+n = int(input("Size of limited buffer?"))
+mutex = threading.Semaphore(value=1)
+empty = threading.Semaphore(value=n)
+full = threading.Semaphore(value=0)
+buffer = []
 if __name__ == '__main__':
-    pool = mp.Pool(mp.cpu_count())
-    n = int(input("Size of limited buffer?"))
-    mutex = threading.Semaphore(value=1)
-    empty = threading.Semaphore(value=n)
-    full = threading.Semaphore(value=0)
-    producerCount = int(input("How many producer?"))
-    consumerCount = int(input("How many consumer?"))
-    buffer = []
-    for i in range(producerCount):
-        pool.apply(producer)
-    for i in range(consumerCount):
-        pool.apply(consumer)
-    pool.close()
-    pool.join()
-    # producer()
-    # consumer()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = {executor.submit(producer), executor.submit(consumer)}
+        concurrent.futures.wait(futures)
